d50587c6eee65201a544ddbf909f4f35d5f8bd59
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2026-09-18 15:59:26 -0400

    Revert "Use patched jupyter-chat, unique jupyter-server-mcp port"
    This reverts commit e909201fdc83731d7e8012f1d52e96692ffc54f6.

--- jupyter-build.sh
+++ jupyter-build.sh
@@ -2,11 +2,8 @@
 
 set -o errexit
 
-# needed to install first to build jupyter-chat from source
-python -mpip install --no-cache-dir nodeenv
-nodeenv --python-virtualenv
-
 python -mpip install --no-cache-dir -r requirements.txt
+nodeenv --python-virtualenv
 
 # ACP adapters for Claude Code and Codex
 # https://jupyter-ai.readthedocs.io/en/latest/getting-started.html#install-agents
